4373af4b4a280448076b2bbd23817f3085eae07d
Mavi-Esquerdo <marciahelenaesquerdo@gmail.com>
2024-02-16 12:27:16 +0000

    Save

---
+++ app/src/main/java/primeiro_exemplo/modelos/Pessoa.java
@@ -0,0 +1,15 @@
+package primeiro_exemplo.modelos;
+
+public class Pessoa {
+    //Atributos 
+public String nome;
+public int idade;
+
+
+
+//Método
+public boolean verificarMaioridade() {
+    return this.idade >= 18;
+    
+    }
+}
